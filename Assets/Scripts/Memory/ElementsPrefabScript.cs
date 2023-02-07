b1822ed11495d8a3028ce98d7b6bb963830c91e4
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2023-02-07 11:18:32 +0100

    new collider layTheTable and memory works

--- Assets/Scripts/Memory/ElementsPrefabScript.cs
+++ Assets/Scripts/Memory/ElementsPrefabScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ElementsPrefabScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         this.name = "Elements";   
     }
